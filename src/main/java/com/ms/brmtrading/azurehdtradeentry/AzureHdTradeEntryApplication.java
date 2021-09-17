bc6cb1d62d89ba1b33237cf6cef72757c8fdc6f9
kocos0 <40714151+kocos0@users.noreply.github.com>
2021-09-17 10:53:21 +0200

    Initial version

---
+++ src/main/java/com/ms/brmtrading/azurehdtradeentry/AzureHdTradeEntryApplication.java
@@ -0,0 +1,13 @@
+package com.ms.brmtrading.azurehdtradeentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureHdTradeEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureHdTradeEntryApplication.class, args);
+	}
+
+}
